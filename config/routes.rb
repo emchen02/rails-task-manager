3d0b9d5364903893fdca24804f9435b7c8b747c5
Emily Chen <emchen02@gmail.com>
2020-11-24 14:43:02 +0800

    done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'tasks/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'index', to: 'tasks#index'
+  get 'index/:id', to: 'tasks#show', as: :show
 end
